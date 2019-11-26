037f17f091d5e0667f010dd036cee1900c62ed25
chenby <chenby@nextop.cn>
2019-11-26 20:41:04 +0800

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * @author Leon Chen
+ * @since 2.6.0
+ */
+module com.moilioncircle.redis.sink.api {
+    exports com.moilioncircle.redis.sink.api;
+    exports com.moilioncircle.redis.sink.api.cmd;
+    exports com.moilioncircle.redis.sink.api.example;
+    exports com.moilioncircle.redis.sink.api.listener;
+    requires org.slf4j;
+    requires com.moilioncircle.redis.replicator;
+}
