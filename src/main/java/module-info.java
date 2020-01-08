2d15bd97ceab5e2eafe340db10217ccaaf661cae
chenby <chenby@nextop.cn>
2020-01-08 11:05:44 +0800

    api

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,3 @@
-/**
- * @author Leon Chen
- * @since 2.6.0
- */
 module com.moilioncircle.redis.sink.api {
     exports com.moilioncircle.redis.sink.api;
     exports com.moilioncircle.redis.sink.api.cmd;
