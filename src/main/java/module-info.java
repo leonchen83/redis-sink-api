dd3b706b9c3805c23fdca5db3468ee61ddabd747
chenby <chenby@nextop.cn>
2020-09-25 14:19:14 +0800

    api

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,19 @@
+import com.moilioncircle.redis.sink.api.ParserService;
+import com.moilioncircle.redis.sink.api.SinkService;
+import com.moilioncircle.redis.sink.api.example.ExampleSinkService;
+import com.moilioncircle.redis.sink.api.parser.DefaultParserService;
+import com.moilioncircle.redis.sink.api.parser.DumpParserService;
+
 module com.moilioncircle.redis.sink.api {
+    requires org.slf4j;
+    requires com.moilioncircle.redis.replicator;
+
     exports com.moilioncircle.redis.sink.api;
     exports com.moilioncircle.redis.sink.api.cmd;
     exports com.moilioncircle.redis.sink.api.parser;
     exports com.moilioncircle.redis.sink.api.example;
     exports com.moilioncircle.redis.sink.api.listener;
-    requires org.slf4j;
-    requires com.moilioncircle.redis.replicator;
+
+    provides SinkService with ExampleSinkService;
+    provides ParserService with DefaultParserService, DumpParserService;
 }
